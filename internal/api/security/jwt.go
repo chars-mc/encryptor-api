eea4dd326c5ed835e710891d061632a75f5c6478
Carlos Manuel <chars.dev@gmail.com>
2021-11-15 18:26:51 -0600

    Implement jwt parse

--- internal/api/security/jwt.go
+++ internal/api/security/jwt.go
@@ -1,6 +1,7 @@
 package security
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/golang-jwt/jwt/v4"
@@ -11,4 +12,13 @@ var secretKey = []byte(os.Getenv("SECRET_KEY"))
 func GenerateToken(claims jwt.Claims) (string, error) {
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(secretKey)
+}
+
+func ParseToken(tokenString string) (*jwt.Token, error) {
+	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
+		}
+		return secretKey, nil
+	})
 }
