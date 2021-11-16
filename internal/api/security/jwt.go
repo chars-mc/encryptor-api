0a13a4a915e7ca6e85fced3c7c499e44ecefc24d
Carlos Manuel <chars.dev@gmail.com>
2021-11-15 22:25:23 -0600

    Move jwt secretkey

--- internal/api/security/jwt.go
+++ internal/api/security/jwt.go
@@ -7,14 +7,14 @@ import (
 	"github.com/golang-jwt/jwt/v4"
 )
 
-var secretKey = []byte(os.Getenv("SECRET_KEY"))
-
 func GenerateToken(claims jwt.Claims) (string, error) {
+	secretKey := []byte(os.Getenv("SECRET_KEY"))
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
 	return token.SignedString(secretKey)
 }
 
 func ParseToken(tokenString string) (*jwt.Token, error) {
+	secretKey := []byte(os.Getenv("SECRET_KEY"))
 	return jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
 		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
 			return nil, fmt.Errorf("Unexpected signing method: %v", token.Header["alg"])
