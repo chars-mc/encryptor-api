51a728b5deeb272520532a0db38f64cb09f856db
Carlos Manuel <chars.dev@gmail.com>
2021-11-15 21:44:15 -0600

    Add domain types

--- internal/encryption/domain/data.go
+++ internal/encryption/domain/data.go
@@ -2,5 +2,39 @@ package domain
 
 type DataType int
 
+var DataTypes = []string{
+	"Text",
+	"File",
+}
+
+const (
+	Text = iota + 1
+	File
+)
+
+func (d DataType) String() string {
+	return DataTypes[d-1]
+}
+
+type Algorithm int
+
+const (
+	AES = iota + 1
+	Blowsifh
+)
+
+var Algorithms = []string{
+	"AES",
+	"Blowsifh",
+}
+
+func (a Algorithm) String() string {
+	return Algorithms[a-1]
+}
+
 type Data struct {
+	ID        int
+	Content   string
+	DataType  DataType
+	Algorithm Algorithm
 }
